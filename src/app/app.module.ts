c51aaa546c9cb6aba74f2a2a45f5b3fab398bfac
Edyson Holguin <edyholguin@uniboyaca.edu.co>
2023-05-24 18:23:39 -0500

    agrego componentes al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { FormularioRegistroComponent } from './formulario-registro/formulario-registro.component';
+import { VentanasConfirmacionComponent } from './ventanas-confirmacion/ventanas-confirmacion.component';
+import { TablaComponent } from './tabla/tabla.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    FormularioRegistroComponent,
+    VentanasConfirmacionComponent,
+    TablaComponent
   ],
   imports: [
     BrowserModule
