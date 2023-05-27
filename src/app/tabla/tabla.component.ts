c3a0873bb6cd82d6a4cc3845cbccb2edb4ee3c15
Edyson Holguin <edyholguin@uniboyaca.edu.co>
2023-05-27 10:23:39 -0500

    creacion tablas

--- src/app/tabla/tabla.component.ts
+++ src/app/tabla/tabla.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './tabla.component.html',
   styleUrls: ['./tabla.component.scss']
 })
-export class TablaComponent {
+  export class TableComponent implements OnInit {
 
-}
+    constructor(private userSS:UserServiceService){
+    }
+
+    usuario: Usuario[] = [];
+
+    ngOnInit(): void {
+      this.usuario = this.userSS.getUsuario();
+    }
+
+    eliminarUsuario(index:number){
+      this.userSS.eliminarUsuario(index);
+    }
+
+  }
+
